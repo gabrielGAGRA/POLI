5cdc31aa91c325a1ee1424c9d01b6c8040a1c1e4
gabriel <69213912+gabrielGAGRA@users.noreply.github.com>
2025-05-27 11:30:27 -0300

    ok

---
+++ LabOO/P1/Copilot/src/model/LivroColecao.java
@@ -0,0 +1,23 @@
+
+package model;
+
+public class LivroColecao extends Livro {
+    public LivroColecao(String t, String a, String p, int y, String i, int pg, String e) {
+        super(t, a, p, y, i, pg, e);
+    }
+
+    @Override
+    public int getDiasEmprestimo() {
+        return 60;
+    }
+
+    @Override
+    public int getMaxRenovacoes() {
+        return 2;
+    }
+
+    @Override
+    public boolean isEmprestavel() {
+        return true;
+    }
+}
---
+++ LabOO/P1/Pos-prova/src/Banco_Usuarios.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Banco_Usuarios {
+    private static List<Usuario> listaUsuarios = new ArrayList<>();
+
+    public static List<Usuario> getListaUsuarios() {
+        return listaUsuarios;
+    }    
+}
